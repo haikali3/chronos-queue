371148f396c089617edafbdf7528ac5fbf2a7900
haikali3 <manfdvcl9@gmail.com>
2026-02-16 03:15:30 +0800

    feat: create jobs table and add initial migration script; add sqlc configuration

--- db/migrations/00001_add_jobs_table.sql
+++ db/migrations/00001_add_jobs_table.sql
@@ -1,9 +1,19 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE jobs (
+  id TEXT PRIMARY KEY,
+  type TEXT NOT NULL,
+  payload BYTEA,
+  status TEXT NOT NULL DEFAULT 'PENDING',
+  retry_count INTEGER NOT NULL DEFAULT 0,
+  max_retries INTEGER NOT NULL DEFAULT 5,
+  idempotency_key TEXT UNIQUE,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE jobs;
 -- +goose StatementEnd
