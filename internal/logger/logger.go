665c26ba4a027f1750c4999dc1683b748ddf71bd
haikali3 <manfdvcl9@gmail.com>
2026-02-16 19:12:02 +0800

    feat: implement logger package with initialization and log level management

---
+++ internal/logger/logger.go
@@ -0,0 +1,60 @@
+package logger
+
+import (
+	"os"
+	"strings"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	cfg   zap.Config
+	level zap.AtomicLevel
+	log   *zap.Logger
+)
+
+func Init() {
+	level = zap.NewAtomicLevel()
+
+	env := os.Getenv("APP_ENV")
+	if env == "production" {
+		cfg = zap.NewProductionConfig()
+		cfg.Level = level
+	} else {
+		cfg = zap.NewDevelopmentConfig()
+		cfg.Level = level
+		log, _ = cfg.Build()
+	}
+	SetLogLevel(os.Getenv("LOG_LEVEL"))
+
+}
+
+func SetLogLevel(levelStr string) {
+	switch strings.ToLower(levelStr) {
+	case "debug":
+		level.SetLevel(zapcore.DebugLevel)
+	case "info":
+		level.SetLevel(zapcore.InfoLevel)
+	case "warn":
+		level.SetLevel(zapcore.WarnLevel)
+	case "error":
+		level.SetLevel(zapcore.ErrorLevel)
+	default:
+		log.Warn("Invalid LOG_LEVEL, defaulting to INFO", zap.String("LOG_LEVEL", levelStr))
+		level.SetLevel(zapcore.InfoLevel)
+	}
+}
+
+func Get() *zap.Logger {
+	if log == nil {
+		Init()
+	}
+	return log
+}
+
+func Sync() {
+	if log != nil {
+		log.Sync()
+	}
+}
