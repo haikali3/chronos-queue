e602410f1576cc4eb32e0922ce9bdaf9ba3a4f3c
haikali3 <manfdvcl9@gmail.com>
2026-02-19 01:04:32 +0800

    feat: define Lock struct for Redis client management

--- internal/storage/redis/lock.go
+++ internal/storage/redis/lock.go
@@ -1,3 +1,15 @@
 package redis
 
-func 
+import (
+	"time"
+
+	goredis "github.com/redis/go-redis/v9"
+)
+
+type Lock struct {
+	client *goredis.Client
+	key    string
+	value  string
+	ttl    time.Duration
+}
+
