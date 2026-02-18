c590409b8eae7be747a7a6e47e913a3533a625cb
haikali3 <manfdvcl9@gmail.com>
2026-02-19 01:26:25 +0800

    feat: update NewLock constructor to generate value automatically and enhance Acquire method

--- internal/storage/redis/lock.go
+++ internal/storage/redis/lock.go
@@ -1,8 +1,11 @@
 package redis
 
 import (
+	"context"
+	"errors"
 	"time"
 
+	"github.com/google/uuid"
 	goredis "github.com/redis/go-redis/v9"
 )
 
@@ -13,11 +16,27 @@ type Lock struct {
 	ttl    time.Duration
 }
 
-func NewLock(client *goredis.Client, key string, value string, ttl time.Duration) *Lock {
+func NewLock(client *goredis.Client, key string, ttl time.Duration) *Lock {
 	return &Lock{
 		client: client,
 		key:    key,
-		value:  value,
+		value:  uuid.NewString(),
 		ttl:    ttl,
 	}
+}
+
+func (l *Lock) Acquire(ctx context.Context) (bool, error) {
+	result := l.client.SetArgs(ctx, l.key, l.value, goredis.SetArgs{
+		Mode: "NX",
+		TTL:  l.ttl,
+	})
+
+	err := result.Err()
+	if err != nil {
+		if errors.Is(err, goredis.Nil) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
