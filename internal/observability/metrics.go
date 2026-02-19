0d118a1468762a4cbf4aba4ba319b5c103026c0b
haikali3 <manfdvcl9@gmail.com>
2026-02-19 17:31:31 +0800

    feat: enhance Metrics struct with Prometheus registration and HTTP handler

--- internal/observability/metrics.go
+++ internal/observability/metrics.go
@@ -1,6 +1,11 @@
 package observability
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
 type Metrics struct {
 	JobsSubmitted    prometheus.Counter
@@ -11,4 +16,57 @@ type Metrics struct {
 	JobDuration      prometheus.Histogram
 	WorkerPoolActive prometheus.Gauge
 	WorkerPoolIdle   prometheus.Gauge
+}
+
+func NewMetrics() *Metrics {
+	m := &Metrics{
+		JobsSubmitted: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "chronos_jobs_submitted_total",
+			Help: "Total number of jobs submitted to the queue",
+		}),
+		JobsCompleted: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "chronos_jobs_completed_total",
+			Help: "Total number of jobs successfully completed",
+		}),
+		JobsFailed: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "chronos_jobs_failed_total",
+			Help: "Total number of jobs that failed",
+		}),
+		JobsRetried: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "chronos_jobs_retried_total",
+			Help: "Total number of job retries",
+		}),
+		QueueDepth: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "chronos_queue_depth",
+			Help: "Current number of jobs in the queue",
+		}),
+		JobDuration: prometheus.NewHistogram(prometheus.HistogramOpts{
+			Name:    "chronos_job_duration_seconds",
+			Help:    "Duration of job processing in seconds",
+			Buckets: prometheus.DefBuckets,
+		}),
+		WorkerPoolActive: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "chronos_worker_pool_active",
+			Help: "Number of active workers in the pool",
+		}),
+		WorkerPoolIdle: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "chronos_worker_pool_idle",
+			Help: "Number of idle workers in the pool",
+		}),
+	}
+	prometheus.MustRegister(
+		m.JobsSubmitted,
+		m.JobsCompleted,
+		m.JobsFailed,
+		m.JobsRetried,
+		m.QueueDepth,
+		m.JobDuration,
+		m.WorkerPoolActive,
+		m.WorkerPoolIdle,
+	)
+	return m
+}
+
+func (m *Metrics) Handler() http.Handler {
+	return promhttp.Handler()
 }
